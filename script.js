a10eab551d89bb7ddf00f5e6c3dce46ea4331ffe
gestok <giorgos.cho@hotmail.com>
2022-10-12 13:30:11 +0300

    3 files modified

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const main = document.querySelector('main');
 const expander = document.querySelector('.expander');
+const search = document.querySelector('.search label');
 
 expander.addEventListener('click', () => {
   main.classList.toggle('open');
+});
+search.addEventListener('click', () => {
+  main.classList.toggle('search');
 });
