ebd64cccdd34c5dcd521b50dd738d46ba00b5ec0
gestok <giorgos.cho@hotmail.com>
2022-10-12 15:47:56 +0300

    3 files modified

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ const menuItems = document.querySelectorAll('.primary .menu-item');
 expander.addEventListener('click', () => main.classList.toggle('open'));
 search.addEventListener('click', () => main.classList.toggle('search'));
 menuItems.forEach((item, i) => {
-  item.addEventListener(
-    'click',
-    () => (current.innerText = item.querySelector('.desc').textContent)
-  );
+  item.addEventListener('click', () => {
+    current.innerText = item.querySelector('.desc').textContent;
+    menuItems.forEach((item) => item.classList.remove('active'));
+    item.classList.add('active');
+  });
 });
