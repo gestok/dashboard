8bdd240cf107002e0b34fdc39a255557e8c4a3d0
gestok <giorgos.cho@hotmail.com>
2022-10-13 19:26:17 +0300

    3 files modified

--- script.js
+++ script.js
@@ -2,18 +2,16 @@ const container = document.querySelector('main');
 const search = document.querySelector('.search label');
 const expander = document.querySelector('main .menu .expander');
 const current = document.querySelector('.current');
-const menuItems = document.querySelectorAll('.primary .menu-item');
-const mainCards = document.querySelectorAll('.dashboard .card');
+const menuItems = document.querySelectorAll('main .menu .primary .menu-item');
+const mainCards = document.querySelectorAll('main .dashboard .card');
 const weatherContent = document.querySelector('.side .weather .content');
 
-expander.addEventListener('click', () => container.classList.toggle('open'));
 search.addEventListener('click', () => container.classList.toggle('search'));
-menuItems.forEach((item, i) => {
+menuItems.forEach((item) => {
   item.addEventListener('click', () => {
     current.innerText = item.querySelector('.desc').textContent;
     menuItems.forEach((item) => item.classList.remove('active'));
     item.classList.add('active');
-    container.classList.remove('open');
   });
 });
 
